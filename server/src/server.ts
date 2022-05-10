2f98370efb40c0b3fb554d98b3e60094424a27ef
Tiago Guatierri <tiagovit@gmail.com>
2022-05-10 14:25:04 -0300

    update cors config

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,11 +5,7 @@ import routes from './routes';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: `localhost:${process.env.PORT || 3333}`,
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use(routes);
